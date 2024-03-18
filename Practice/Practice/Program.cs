453c7a3c5abc06ba89713429445caba07261e5ca
franklinnot <jhonfrank479@gmail.com>
2024-03-18 00:29:30 -0500

    resolve

--- Practice/Practice/Program.cs
+++ Practice/Practice/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hola Yuli-Eisy");
-string edad=Console.ReadLine()
+string edad = Console.ReadLine();
 Console.WriteLine($"Tienes{edad}");
+Console.WriteLine("Que onda perris");
