9d50b3f0e4558cdec9da098ab456db82d831dc9c
franklinnot <jhonfrank479@gmail.com>
2024-03-18 17:27:31 -0500

    num 1

--- Practice/Practice/Program.cs
+++ Practice/Practice/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Esto es una prueba para revertir cambios");
 Console.WriteLine("Este número empezará en 5 e ira descendiendo hasta 1 en cada commit");
-Console.WriteLine("3");
+Console.WriteLine("1");
