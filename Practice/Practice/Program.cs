6def527b76a8f5e703655485cf195fa3ee3056fc
Levi <leonardobenites282@gmail.com>
2024-03-18 00:17:58 -0500

    actualizacion
    error

--- Practice/Practice/Program.cs
+++ Practice/Practice/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hola Yuli-Eisy");
-string edad=Console.ReadLine();
+string edad=Console.ReadLine()
 Console.WriteLine($"Tienes{edad}");
