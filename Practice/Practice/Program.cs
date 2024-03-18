e17b3e4ee14410d2f6744b5b5f91212da898a735
franklinnot <jhonfrank479@gmail.com>
2024-03-18 17:16:51 -0500

    duh

--- Practice/Practice/Program.cs
+++ Practice/Practice/Program.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+<<<<<<< HEAD
 Console.WriteLine("Hola Yuli-Eisy");
 string edad = Console.ReadLine();
 Console.WriteLine($"Tienes{edad}");
+=======
+>>>>>>> parent of 0dff0d7 (Edad)
 Console.WriteLine("Que onda perris");
