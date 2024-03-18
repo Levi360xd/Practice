7e7720d3e5ed8efe3af9f8f32a72260d0d5e68ce
franklinnot <jhonfrank479@gmail.com>
2024-03-18 17:28:27 -0500

    linea eliminada

--- Practice/Practice/Program.cs
+++ Practice/Practice/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Esto es una prueba para revertir cambios");
 Console.WriteLine("Este número empezará en 5 e ira descendiendo hasta 1 en cada commit");
-Console.WriteLine("1");
+
+Console.WriteLine("Llego a cero, eliminamos la linea y creamos esta");
