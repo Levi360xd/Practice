db0d0165bb2c65fcdf79d6b3e9ed7674dcd7c781
franklinnot <jhonfrank479@gmail.com>
2024-03-18 17:20:23 -0500

    po

--- Practice/Practice/Program.cs
+++ Practice/Practice/Program.cs
@@ -1,8 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-<<<<<<< HEAD
-Console.WriteLine("Hola Yuli-Eisy");
-string edad = Console.ReadLine();
-Console.WriteLine($"Tienes{edad}");
-=======
->>>>>>> parent of 0dff0d7 (Edad)
 Console.WriteLine("Que onda perris");
+Console.WriteLine("Hello, World!");
